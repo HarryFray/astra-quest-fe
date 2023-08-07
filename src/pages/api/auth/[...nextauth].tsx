4f2a1bd6e55568094eeb392dce88ecf470837371
nfray <harry.fray7@gmail.com>
2023-08-07 15:27:39 -0600

    poc sso

---
+++ src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  secret: process.env.SECRET,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // OAuth authentication providers
+    // AppleProvider({
+    //   clientId: process.env.APPLE_ID,
+    //   clientSecret: process.env.APPLE_SECRET,
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // }),
+    // // Sign in with passwordless email link
+    // EmailProvider({
+    //   server: process.env.MAIL_SERVER,
+    //   from: "<no-reply@example.com>",
+    // }),
+  ],
+});
