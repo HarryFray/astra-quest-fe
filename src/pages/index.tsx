200384b73f729c6a50c299a47ea029f4118b5ec8
nfray <harry.fray7@gmail.com>
2023-08-07 15:56:53 -0600

    minor updates to styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,33 @@
-import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
   const { data: session } = useSession();
+
   return (
-    <main
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "70vh",
-      }}
-    >
-      <div className="header">
-        <Link href="/">
-          <p className="logo">NextAuth.js</p>
-        </Link>
-        {session && (
-          <Link href="#" onClick={() => signOut()} className="btn-signin">
-            Sign out
-          </Link>
-        )}
-        {!session && (
-          <Link href="#" onClick={() => signIn()} className="btn-signin">
-            Sign in
-          </Link>
-        )}
+    <main className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="flex flex-col items-center p-8 bg-white rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">AstroQuest</h1>
+        <p className="text-lg text-gray-600 text-center mb-8">
+          Embark on an epic space journey to explore the cosmos and uncover its
+          mysteries.
+        </p>
+        <div className="mt-4">
+          {session ? (
+            <button
+              onClick={() => signOut()}
+              className="px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700"
+            >
+              Sign out
+            </button>
+          ) : (
+            <button
+              onClick={() => signIn()}
+              className="px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700"
+            >
+              Sign in
+            </button>
+          )}
+        </div>
       </div>
     </main>
   );
