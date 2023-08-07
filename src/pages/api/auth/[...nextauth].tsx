40b6ef4ad919f625bf8ac9cc1d679d92c7a2471f
nfray <harry.fray7@gmail.com>
2023-08-07 15:43:37 -0600

    sign in with google

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   secret: process.env.SECRET,
@@ -8,14 +9,14 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
     // OAuth authentication providers
     // AppleProvider({
     //   clientId: process.env.APPLE_ID,
     //   clientSecret: process.env.APPLE_SECRET,
-    // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID,
-    //   clientSecret: process.env.GOOGLE_SECRET,
     // }),
     // // Sign in with passwordless email link
     // EmailProvider({
@@ -23,4 +24,7 @@ export default NextAuth({
     //   from: "<no-reply@example.com>",
     // }),
   ],
+  session: {
+    jwt: true,
+  },
 });
