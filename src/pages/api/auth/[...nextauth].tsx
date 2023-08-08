5e2c3b0ca3fcccdb44455ddaa52bed6816b02cb4
nfray <harry.fray7@gmail.com>
2023-08-07 21:02:12 -0600

    code clean up

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -3,7 +3,6 @@ import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-  secret: process.env.SECRET,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
