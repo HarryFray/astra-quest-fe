5b0decc2f17ac6a3d3941226624acbfb1b969b96
nfray <harry.fray7@gmail.com>
2023-08-08 07:44:48 -0600

    display table with all astro folk

--- src/pages/astronauts.tsx
+++ src/pages/astronauts.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import axios from "axios";
+
+interface IAstronaut {
+  name: string;
+  craft: string;
+}
 
 const AstronautsPage = () => {
+  const [allAstronaut, setAllAstronaut] = useState<IAstronaut[]>([]);
   const { data: session, status } = useSession();
   const router = useRouter();
+
+  useEffect(() => {
+    async function fetchAllAstronaut() {
+      try {
+        const response = await axios.get(
+          "http://api.open-notify.org/astros.json"
+        );
+        const data = response.data;
+        setAllAstronaut(data.people);
+      } catch (error) {
+        console.error("Error fetching astronauts data:", error);
+      }
+    }
+
+    if (status === "authenticated") {
+      fetchAllAstronaut();
+    }
+  }, [status]);
 
   // TODO: SHOULD BE ELIVATED TO A HIGHER ORDER COMPONENT
   if (status === "loading") {
@@ -29,6 +55,17 @@ const AstronautsPage = () => {
           Embark on an epic journey to explore the cosmos and uncover its
           mysteries.
         </p>
+        <div className="mt-8 w-full">
+          {allAstronaut.map((astronaut, index) => (
+            <div
+              key={index}
+              className="flex justify-between px-4 py-2 border-b border-gray-300"
+            >
+              <p>{astronaut.name}</p>
+              <p>{astronaut.craft}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
